51187ff6eae5ae2ca37a0152bfbe784f96cd8ad1
Mbilallayers <muhammadb083@yahoo.com>
2022-08-30 11:10:29 +0500

    change whole template design to new cassie tech team

---
+++ src/app/components/pages/home-demo-one/homeone-featured/homeone-featured.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homeone-featured',
+  templateUrl: './homeone-featured.component.html',
+  styleUrls: ['./homeone-featured.component.scss']
+})
+export class HomeoneFeaturedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
