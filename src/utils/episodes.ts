dea4596d26d1f9963d8fcf2a6001066deb837476
Mbilallayers <muhammadb083@yahoo.com>
2022-12-07 14:10:51 +0500

    add episode details to detail section

--- src/utils/episodes.ts
+++ src/utils/episodes.ts
@@ -7,7 +7,108 @@ export const episodesList = [
     artist: 'Cassandra Cooper',
     flyerUrl: 'assets/images/v2/Talk_Tech_to_Me_NicolethePole_episode%20copy%202.png',
     link: 'assets/audio/Episode0.1-Tech Pilot2-ReneeMoncito.mp3',
-    description: ''
+    description: `
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      <strong>adipisicing</strong> elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      <a routerLink="/">commodo</a> consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <ol>
+      <li>
+        <p>Nunc, mauris ut in vestibulum. Consectetur phasellus ultrices
+        fusce nibh justo, venenatis, amet. Lectus quam in</p>
+      </li>
+      <li>
+        <p>Most of the designer are very creative to do something , mauris ut
+        in vestibulum. Consectetur phasellus ultrices fusce nibh justo,
+        venenatis, amet. Lectus quam in lobortis.</p>
+      </li>
+      <li>
+        <p>There are two thing is very important in Consectetur phasellus
+        ultrices fusce nibh justo, venenatis, amet. Lectus quam.</p>
+      </li>
+      <li>
+        <p>Web design and development very creative to do something, Mauris
+        ut in vestibulum. Consectetur phasellus ultrices fusce nibh
+        venenatis, amet to all design and development.</p>
+      </li>
+    </ol>
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-2.jpg" alt="image" />
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-3.jpg" alt="image" />
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-4.jpg" alt="image" />
+    </p>
+    <h3>Four major elements that we offer:</h3>
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <ul>
+      <li><p>Scientific skills for getting a better result</p></li>
+      <li><p>Communication skills to getting in touch</p></li>
+      <li><p>A career overview opportunity is available</p></li>
+      <li><p>A good work environment for work</p></li>
+    </ul>
+    <h3>Setting the mood with incense</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <blockquote>
+      <p>
+        It is a long-established fact that a reader will be distracted by
+        the readable content of a page when looking at its layout.
+      </p>
+    </blockquote>
+    <h3>The rise of marketing and why you need it</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      ad minim veniam, quis nostrud.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      <strong>adipisicing</strong> elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      <a routerLink="/">commodo</a> consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <p>
+      <strong>TAGS:</strong>
+      <a routerLink="/">Music</a>,
+      <a routerLink="/">Learning</a>
+    </p>
+    <p>
+      <strong>Share:</strong>
+      <a href="https://facebook.com/">Facebook</a>,
+      <a href="https://twitter.com/">Twitter</a>
+    </p>
+    `
   },{
     number: '0.1',
     title: 'Systems and processes for a successful business',
@@ -16,7 +117,108 @@ export const episodesList = [
     artist: 'Renee Moncito',
     flyerUrl: 'assets/images/v2/Talk_Tech_to_Me_NicolethePole_episode%20copy%202.png',
     link: 'assets/audio/Episode0.1-Tech Pilot2-ReneeMoncito.mp3',
-    description: ''
+    description: `
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      <strong>adipisicing</strong> elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      <a routerLink="/">commodo</a> consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <ol>
+      <li>
+        <p>Nunc, mauris ut in vestibulum. Consectetur phasellus ultrices
+        fusce nibh justo, venenatis, amet. Lectus quam in</p>
+      </li>
+      <li>
+        <p>Most of the designer are very creative to do something , mauris ut
+        in vestibulum. Consectetur phasellus ultrices fusce nibh justo,
+        venenatis, amet. Lectus quam in lobortis.</p>
+      </li>
+      <li>
+        <p>There are two thing is very important in Consectetur phasellus
+        ultrices fusce nibh justo, venenatis, amet. Lectus quam.</p>
+      </li>
+      <li>
+        <p>Web design and development very creative to do something, Mauris
+        ut in vestibulum. Consectetur phasellus ultrices fusce nibh
+        venenatis, amet to all design and development.</p>
+      </li>
+    </ol>
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-2.jpg" alt="image" />
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-3.jpg" alt="image" />
+    </p>
+    <p>
+      <img src="assets/images/blog/blog-4.jpg" alt="image" />
+    </p>
+    <h3>Four major elements that we offer:</h3>
+    <p>
+      Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+      Neque porro quia non numquam eius modi tempora incidunt ut labore et
+      dolore magnam dolor sit amet, consectetur adipisicing.
+    </p>
+    <ul>
+      <li><p>Scientific skills for getting a better result</p></li>
+      <li><p>Communication skills to getting in touch</p></li>
+      <li><p>A career overview opportunity is available</p></li>
+      <li><p>A good work environment for work</p></li>
+    </ul>
+    <h3>Setting the mood with incense</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <blockquote>
+      <p>
+        It is a long-established fact that a reader will be distracted by
+        the readable content of a page when looking at its layout.
+      </p>
+    </blockquote>
+    <h3>The rise of marketing and why you need it</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      ad minim veniam, quis nostrud.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      <strong>adipisicing</strong> elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      <a routerLink="/">commodo</a> consequat. Duis aute irure dolor in
+      reprehenderit in sed quia non numquam eius modi tempora incidunt ut
+      labore et dolore magnam aliquam quaerat.
+    </p>
+    <p>
+      <strong>TAGS:</strong>
+      <a routerLink="/">Music</a>,
+      <a routerLink="/">Learning</a>
+    </p>
+    <p>
+      <strong>Share:</strong>
+      <a href="https://facebook.com/">Facebook</a>,
+      <a href="https://twitter.com/">Twitter</a>
+    </p>
+    `
   },{
     number: '1',
     title: 'Black folk in the metaverse',
@@ -26,11 +228,245 @@ export const episodesList = [
     flyerUrl: 'assets/images/v2/Talk_Tech_to_Me_CASSIExFLIP_2.jpg',
     link: 'assets/audio/Episode0.1-Tech Pilot2-ReneeMoncito.mp3',
     description: `
-      <h1 class="detail-main-title">12:03:00 pm - 12:10:35 pm (Segment 1)</h1>
-      <p class="detail-description">Show intro: Welcome forward, I am your host Cassie Betts on KBLA Talk 1580 and you’re listening to Talk Tech To Me, where Technology is the new sexy. Black Geeks are the new superheroes and South Central LA is the new Tech Mecca. </p>
-      <ul class="detail-list">
-        <li>A brief overview of the show (purpose + goal)</li>
-        <li>About me</li>
+      <h1>12:03:00 pm - 12:10:35 pm (Segment 1)</h1>
+      <p>
+        Show intro: Welcome forward, I am your host Cassie Betts on 
+        KBLA Talk 1580 and you’re listening to 
+        Talk Tech To Me, where Technology is the new sexy. Black 
+        Geeks are the new superheroes and South Central LA is the new Tech Mecca.
+      </p>
+      <ul>
+        <li>
+          <p>A brief overview of the show (purpose + goal)</p>
+        </li>
+        <li>
+          <p>About me</p>
+        </li>
+      </ul>
+      <h1>What’s the Tea in Tech</h1>
+      <ul>
+        <li>
+          <p>
+            <a href="https://youtu.be/TDDFLxZ_xDA" target="_blank">Chris Breezy Takes His Talents to the Metaverse</a>:  Award-winning R&B and hip hop cultural icon 
+              Chris Brown is ready to play in the metaverse. The “Warm Embrace” singer 
+              dropped his first official NFT collection THE BREEZY VERSE in late June.
+          </p>
+          <ul>
+            <li>
+              <p>The drop features a series of 10,000 3D animated NFTs w/ sounds & visual effects that will live on the Ethereum blockchain</p>
+            </li>
+            <li>
+              <p>Investors and fans will get access to exclusive utilities including VIP concert tickets, 
+              meet & greets, video calls, personal wardrobe, and 1 of 1 
+              original art on canvas by Chris. Learn more at <a href="https://www.thebreezyverse.com/" target="_blank">thebreezyverse.com</a></p>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <p>
+            <a href="https://us.nothing.tech/pages/phone-1" target="_blank">Move Over Apple and Samsung, Nothing is Here</a>: Nothing, the London-based startup 
+            dubbed  “The most hyped tech company in years,” by 
+            GQ, has a lot to prove with the release of their Nothing Phone (1).
+          </p>
+          <ul>
+            <li>
+              <p>The mid-range android is said to be the biggest smartphone launch of 2022 and is popular for its unique design and decent price point</p>
+            </li>
+            <li>
+              <p>Features include a see-through glass back panel with glyph lighting, dual camera, & wireless charging. 
+                Available in 3 variants; 8-12GB of RAM + up to 256GB of storage; Price: £399 (~$475)- £449 (~$535)</p>
+            </li>
+            <li>
+              <p>
+                Limited release in the UK this week w/ open sales in Europe, Asia and 40+ countries on 7/21; Not currently 
+                available in the US but possibly in the future
+              </p>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <p>
+            <a href="https://us.nothing.tech/pages/phone-1" target="_blank">Sunday, July 17th is World Emoji Day:</a> Emojis have 
+            changed the text game forever so it’s only right we celebrate them
+          </p>
+          <ul>
+            <li>
+              <p>July 17 is famously displayed on the 📅 Calendar Emoji, which is why it was chosen as the date for World Emoji Day</p>
+              <ul>
+                <li>
+                  <p>Quick Stats: Over 900 million emojis are sent everyday without text on Facebook Messenger</p>
+                </li>
+                <li>
+                  <p>100 Tweets per minute are sent using the #worldemojiday hashtag</p>
+                </li>
+                <li>
+                  <p>In 2021, the World Emoji Awards crowned the 💉syringe emoji the “Most 2021 Emoji”</p>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <p>Follow @EmojiAwards on Twitter July 17th to see what new and upcoming emoji get crowned for 2022!</p>
+            </li>
+            <li>
+              <p>
+                <em>Soft out with a commercial break: 3 minutes</em>
+              </p>
+            </li>
+            <li>
+              <p>
+                <em>He won’t go to break until she is ready</em>
+              </p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <h1>12:13:35 pm - 12:30:00 pm (Segment 2)</h1>
+      <ul>
+        <li>
+          <p>Introduce special guest Jeremiah “Flip” Benjamin, Metaverse Architect and Founder of 88 Ideas and Pod Labs</p>
+        </li>
+        <li>
+          <p><em>TTTM Icebreaker:</em> When did you fall in love with Tech?</p>
+        </li>
+        <li>
+          <p>What is the metaverse & who is building it?</p>
+        </li>
+        <li>
+          <p>How can people access the metaverse?</p>
+        </li>
+        <li>
+          <p>What are the pros and cons of the metaverse and similar virtual spaces?</p>
+        </li>
+        <li>
+          <p>How can the metaverse and NFT’s positively affect Black economic conditions?</p>
+        </li>
+        <li>
+          <p>How do you see Black culture impacting this space?</p>
+        </li>
+        <li>
+          <p>What impact will this have on everyday life & what might this look like 10-20 years from now?</p>
+        </li>
+        <li>
+          <p>What would you say to Web3 skeptics, those on the fence about the metaverse, NFTs, crypto, and other emerging technologies?</p>
+          <ul>
+            <li>
+              <p><em>Hard Out at the 30 (bottom of the hour) with a 6-minute break</em></p>
+            </li>
+            <li>
+              <p><em>News and traffic break - 3 minutes</em></p>
+            </li>
+            <li>
+              <p><em>Commercial break - 3 minutes</em></p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <h1>12:36:00 pm - 12:45:45 pm (Segment 3)</h1>
+      <ul>
+        <li>
+          <p>What role does being a South Central LA native play in your creative process and the way you approach projects?</p>
+        </li>
+        <li>
+          <p>What are some of the most satisfying projects you’ve worked on?</p>
+        </li>
+        <li>
+          <p>What is your <em>“why”</em> for starting your design company 88 Ideas?</p>
+        </li>
+        <li>
+          <p>How can creators protect their intellectual property in the metaverse and ensure they're being properly compensated for their digital art?</p>
+        </li>
+        <li>
+          <p>What was the biggest challenge you have faced and how did you overcome it?</p>
+        </li>
+        <li>
+          <p>What are some Web3/design learning resources and conferences listeners can attend to learn more?</p>
+          <ul>
+            <li>
+              <p><em>Commercial break - 3 minutes</em></p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <h1>12:48:45 pm - 12:53:00 pm (Segment 4)</h1>
+      <ul>
+        <li>
+          <p>What has helped you get to where you are and what advice would you have for others who want to set off in a similar direction?</p>
+        </li>
+        <li>
+          <p>What’s the question you are most tired of hearing on this subject, and what would you like to say about it, so you never have to answer it again?</p>
+          <ul>
+            <li>
+              <p><em>Commercial break - 3 minutes</em></p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <h1>12:56:00 pm - 1:00:00 pm (Segment 5)</h1>
+      <p>Use this to wrap up any last points, call to action, opportunities, etc.</p>
+      <ul>
+        <li>
+          <p>Promote MISLA Billionaire Bootcamps (For Billionaire Bootcamps follow @therealmisla)</p>
+        </li>
+        <li>
+          <p>Talk to Flip about doing a Metaverse Bootcamp</p>
+        </li>
+        <li>
+          <p>Tease details for the next show</p>
+        </li>
+        <li>
+          <p>Direct listeners to connect with Flip and me on social media, company/personal websites, and at any upcoming events</p>
+          <ul>
+            <li>
+              <p>Flip, how can the people connect w/ you? Drop SM!</p>
+            </li>
+            <li>
+              <p>Follow @talktechtomepod on IG</p>
+            </li>
+            <li>
+              <p>Follow me @instabetts</p>
+            </li>
+            <li>
+              <p>To learn more about MISLA/MARTY go to misla.org</p>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <p>
+            Thank you for hanging with us at the Unapologetically Progressive KBLA Talk 1580. When we come forward, you’ll be 
+            listening to The Back Story with Mischa Duffy. Backstory of films, songs, books…
+          </p>
+        </li>
+        <li>
+          <p>
+            TTTM Closing: This has been Talk Tech To Me, pushing the envelope of tech one keystroke at a time, peace out!
+          </p>
+        </li>
+      </ul>
+      <p>Tag Lines: Say as much as possible. </p>
+      <ul>
+        <li>
+          <p>KBLA Talk 1580, we’ve got a lot to talk about.</p>
+        </li>
+        <li>
+          <p>Unapologetically Progressive KBLA Talk 1580.</p>
+          <ul>
+            <li>
+              <p>No CURSING!</p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p>Retrospective:</p>
+      <ul>
+        <li>
+          <p>Don’t call the guests “co-hosts”</p>
+        </li>
+        <li>
+          <p>When guest comes on, part of the convo we can geek out, but most of the convo has to be had as though we are explaining it to a 5 year old</p>
+        </li>
+        <li>
+          <p>To get guests to call in, don’t ask them to call, just say, Welcome Forward, 800-920-1580…</p>
+        </li>
       </ul>
     `
   }
